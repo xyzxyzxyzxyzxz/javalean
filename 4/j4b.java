dccd079bb577a297d59d93a13157c6a121569176
unknown <xyzxyzxyzxyzxz@gmail.com>
2016-02-06 20:51:17 +0800

    sc

---
+++ 4/j4b.java
@@ -0,0 +1,25 @@
+import java.lang.Math.*;
+
+public class j4b{
+	public static void main (String args[]){
+		int ii=1;
+		for (ii=1;ii<10;ii++){
+			switch (ii){
+			case 1:
+			{
+				System.out.println("1");
+				//System.out.println("11");
+				break;
+			}
+			case 2:
+			{
+				System.out.println("2");
+				//System.out.println("22");
+				break;
+			}
+			default:
+				System.out.println("Others");
+			}
+		}
+	}
+}
