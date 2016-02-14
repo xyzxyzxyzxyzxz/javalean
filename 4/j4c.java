f822c0d3d91d138fb13136d82c151dbba7a0fcc1
unknown <xyzxyzxyzxyzxz@gmail.com>
2016-02-14 20:08:33 +0800

    tc

---
+++ 4/j4c.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class j4c {
+	public static void main (String args[]) {
+		
+		double re;
+		
+		System.out.println("hw");
+		
+	re=Math.abs(-5.888);XZ^TScvg777d
+	
+	
+	System.out.println(re);
+	
+	
+	
+	}
+}
